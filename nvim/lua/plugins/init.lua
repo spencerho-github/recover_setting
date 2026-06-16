45c28835a5384362986bdd42a0d35c200a38d3b8
spencer <woshiheyongjun@gmail.com>
2026-06-16 11:52:11 +0800

    Use Neovim 0.12+ native vim.pack.add() API

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,82 +1,65 @@
 -- ============================================================================
--- Neovim 原生插件管理（使用 pack 系统）
+-- Neovim 0.12+ 原生插件管理（使用 vim.pack）
 -- ============================================================================
 
--- 插件安装路径
-local plugin_path = vim.fn.stdpath("data") .. "/site/pack/plugins/start/"
+-- 定义插件列表
+local plugins = {
+  -- 主题
+  "projekt0n/github-nvim-theme",
 
--- 确保插件目录存在
-vim.fn.mkdir(plugin_path, "p")
+  -- 文件浏览
+  "nvim-tree/nvim-tree.lua",
+  "nvim-tree/nvim-web-devicons",
 
--- 辅助函数：确保插件已安装
-local function ensure_plugin(repo, name)
-  local install_path = plugin_path .. name
-  if vim.fn.isdirectory(install_path) == 0 then
-    print("Installing " .. name .. "...")
-    vim.fn.system({
-      "git",
-      "clone",
-      "--depth=1",
-      "https://github.com/" .. repo,
-      install_path,
-    })
-    vim.cmd("packloadall")
-    return true
-  end
-  return false
+  -- 模糊查找
+  "nvim-telescope/telescope.nvim",
+  "nvim-lua/plenary.nvim",
+
+  -- 语法高亮
+  "nvim-treesitter/nvim-treesitter",
+
+  -- LSP
+  "neovim/nvim-lspconfig",
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+
+  -- 自动补全
+  "hrsh7th/nvim-cmp",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-path",
+  "L3MON4D3/LuaSnip",
+  "saadparwaiz1/cmp_luasnip",
+
+  -- 状态栏
+  "nvim-lualine/lualine.nvim",
+
+  -- Git
+  "lewis6991/gitsigns.nvim",
+
+  -- 注释
+  "numToStr/Comment.nvim",
+
+  -- 自动配对
+  "windwp/nvim-autopairs",
+
+  -- 缩进线
+  "lukas-reineke/indent-blankline.nvim",
+
+  -- 通知
+  "rcarriga/nvim-notify",
+
+  -- 快捷键提示
+  "folke/which-key.nvim",
+}
+
+-- 使用 vim.pack.add() 添加插件
+for _, plugin in ipairs(plugins) do
+  vim.pack.add({
+    name = plugin:match("([^/]+)$"),
+    url = "https://github.com/" .. plugin,
+  })
 end
-
--- ============================================================================
--- 安装插件
--- ============================================================================
-
--- 主题
-ensure_plugin("projekt0n/github-nvim-theme", "github-nvim-theme")
-
--- 文件浏览
-ensure_plugin("nvim-tree/nvim-tree.lua", "nvim-tree.lua")
-ensure_plugin("nvim-tree/nvim-web-devicons", "nvim-web-devicons")
-
--- 模糊查找
-ensure_plugin("nvim-telescope/telescope.nvim", "telescope.nvim")
-ensure_plugin("nvim-lua/plenary.nvim", "plenary.nvim")
-
--- 语法高亮
-ensure_plugin("nvim-treesitter/nvim-treesitter", "nvim-treesitter")
-
--- LSP
-ensure_plugin("neovim/nvim-lspconfig", "nvim-lspconfig")
-ensure_plugin("williamboman/mason.nvim", "mason.nvim")
-ensure_plugin("williamboman/mason-lspconfig.nvim", "mason-lspconfig.nvim")
-
--- 自动补全
-ensure_plugin("hrsh7th/nvim-cmp", "nvim-cmp")
-ensure_plugin("hrsh7th/cmp-nvim-lsp", "cmp-nvim-lsp")
-ensure_plugin("hrsh7th/cmp-buffer", "cmp-buffer")
-ensure_plugin("hrsh7th/cmp-path", "cmp-path")
-ensure_plugin("L3MON4D3/LuaSnip", "LuaSnip")
-ensure_plugin("saadparwaiz1/cmp_luasnip", "cmp_luasnip")
-
--- 状态栏
-ensure_plugin("nvim-lualine/lualine.nvim", "lualine.nvim")
-
--- Git
-ensure_plugin("lewis6991/gitsigns.nvim", "gitsigns.nvim")
-
--- 注释
-ensure_plugin("numToStr/Comment.nvim", "Comment.nvim")
-
--- 自动配对
-ensure_plugin("windwp/nvim-autopairs", "nvim-autopairs")
-
--- 缩进线
-ensure_plugin("lukas-reineke/indent-blankline.nvim", "indent-blankline.nvim")
-
--- 通知
-ensure_plugin("rcarriga/nvim-notify", "nvim-notify")
-
--- 快捷键提示
-ensure_plugin("folke/which-key.nvim", "which-key.nvim")
 
 -- ============================================================================
 -- 插件配置
