9cf25631455c6d10a21ca4d3944b8ca05a62b04d
spencer <woshiheyongjun@gmail.com>
2026-06-16 11:58:26 +0800

    Fix: Use 'src' field instead of 'url' for vim.pack.add()

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -5,52 +5,52 @@
 -- 使用 vim.pack.add() 添加插件
 vim.pack.add({
   -- 主题
-  { name = "github-nvim-theme", url = "https://github.com/projekt0n/github-nvim-theme" },
+  { name = "github-nvim-theme", src = "https://github.com/projekt0n/github-nvim-theme" },
 
   -- 文件浏览
-  { name = "nvim-tree.lua", url = "https://github.com/nvim-tree/nvim-tree.lua" },
-  { name = "nvim-web-devicons", url = "https://github.com/nvim-tree/nvim-web-devicons" },
+  { name = "nvim-tree.lua", src = "https://github.com/nvim-tree/nvim-tree.lua" },
+  { name = "nvim-web-devicons", src = "https://github.com/nvim-tree/nvim-web-devicons" },
 
   -- 模糊查找
-  { name = "telescope.nvim", url = "https://github.com/nvim-telescope/telescope.nvim" },
-  { name = "plenary.nvim", url = "https://github.com/nvim-lua/plenary.nvim" },
+  { name = "telescope.nvim", src = "https://github.com/nvim-telescope/telescope.nvim" },
+  { name = "plenary.nvim", src = "https://github.com/nvim-lua/plenary.nvim" },
 
   -- 语法高亮
-  { name = "nvim-treesitter", url = "https://github.com/nvim-treesitter/nvim-treesitter" },
+  { name = "nvim-treesitter", src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 
   -- LSP
-  { name = "nvim-lspconfig", url = "https://github.com/neovim/nvim-lspconfig" },
-  { name = "mason.nvim", url = "https://github.com/williamboman/mason.nvim" },
-  { name = "mason-lspconfig.nvim", url = "https://github.com/williamboman/mason-lspconfig.nvim" },
+  { name = "nvim-lspconfig", src = "https://github.com/neovim/nvim-lspconfig" },
+  { name = "mason.nvim", src = "https://github.com/williamboman/mason.nvim" },
+  { name = "mason-lspconfig.nvim", src = "https://github.com/williamboman/mason-lspconfig.nvim" },
 
   -- 自动补全
-  { name = "nvim-cmp", url = "https://github.com/hrsh7th/nvim-cmp" },
-  { name = "cmp-nvim-lsp", url = "https://github.com/hrsh7th/cmp-nvim-lsp" },
-  { name = "cmp-buffer", url = "https://github.com/hrsh7th/cmp-buffer" },
-  { name = "cmp-path", url = "https://github.com/hrsh7th/cmp-path" },
-  { name = "LuaSnip", url = "https://github.com/L3MON4D3/LuaSnip" },
-  { name = "cmp_luasnip", url = "https://github.com/saadparwaiz1/cmp_luasnip" },
+  { name = "nvim-cmp", src = "https://github.com/hrsh7th/nvim-cmp" },
+  { name = "cmp-nvim-lsp", src = "https://github.com/hrsh7th/cmp-nvim-lsp" },
+  { name = "cmp-buffer", src = "https://github.com/hrsh7th/cmp-buffer" },
+  { name = "cmp-path", src = "https://github.com/hrsh7th/cmp-path" },
+  { name = "LuaSnip", src = "https://github.com/L3MON4D3/LuaSnip" },
+  { name = "cmp_luasnip", src = "https://github.com/saadparwaiz1/cmp_luasnip" },
 
   -- 状态栏
-  { name = "lualine.nvim", url = "https://github.com/nvim-lualine/lualine.nvim" },
+  { name = "lualine.nvim", src = "https://github.com/nvim-lualine/lualine.nvim" },
 
   -- Git
-  { name = "gitsigns.nvim", url = "https://github.com/lewis6991/gitsigns.nvim" },
+  { name = "gitsigns.nvim", src = "https://github.com/lewis6991/gitsigns.nvim" },
 
   -- 注释
-  { name = "Comment.nvim", url = "https://github.com/numToStr/Comment.nvim" },
+  { name = "Comment.nvim", src = "https://github.com/numToStr/Comment.nvim" },
 
   -- 自动配对
-  { name = "nvim-autopairs", url = "https://github.com/windwp/nvim-autopairs" },
+  { name = "nvim-autopairs", src = "https://github.com/windwp/nvim-autopairs" },
 
   -- 缩进线
-  { name = "indent-blankline.nvim", url = "https://github.com/lukas-reineke/indent-blankline.nvim" },
+  { name = "indent-blankline.nvim", src = "https://github.com/lukas-reineke/indent-blankline.nvim" },
 
   -- 通知
-  { name = "nvim-notify", url = "https://github.com/rcarriga/nvim-notify" },
+  { name = "nvim-notify", src = "https://github.com/rcarriga/nvim-notify" },
 
   -- 快捷键提示
-  { name = "which-key.nvim", url = "https://github.com/folke/which-key.nvim" },
+  { name = "which-key.nvim", src = "https://github.com/folke/which-key.nvim" },
 })
 
 -- ============================================================================
